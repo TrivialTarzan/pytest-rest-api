da3d10c97c7a87a860a986aa0d96c3ed58f66363
mstolarski <szuminpawel@gmail.com>
2025-02-24 15:11:07 +0100

    chenged config

--- config/config_loader.py
+++ config/config_loader.py
@@ -1,7 +1,8 @@
 import yaml
 import os
 
-def load_config():
+def load():
     config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
     with open(config_path, "r") as file:
         return yaml.safe_load(file)
+    

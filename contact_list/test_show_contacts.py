a64a8acfd4296acbdc5aba497728ef4b74133f8a
Pawel Szumin <szuminpawel@gmail.com>
2025-02-27 13:16:44 +0100

    changed logging style

--- contact_list/test_show_contacts.py
+++ contact_list/test_show_contacts.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 def test_get_contact_list(api_client, endpoint_get_contact_list, login_and_logout):
     payload = {}
     headers = {
@@ -6,6 +10,11 @@ def test_get_contact_list(api_client, endpoint_get_contact_list, login_and_logou
     
     response = api_client.get(endpoint_get_contact_list, headers, payload)
     data = response.json()
+    
+    for i in range(0, len(data)):
+        logger.info(f"Contact {i+1}:")
+        for detail in data[i]:
+            logger.info(f"\t{detail}: {data[i][detail]}")
     
     assert isinstance(data, list), "Response is not a list"
     assert data[0]['firstName'] == 'Carlos', f"Expected first name 'Carlos', but got {data[0]['firstName']}"
